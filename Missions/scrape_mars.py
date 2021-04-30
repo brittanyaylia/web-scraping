0528407939fb7b7d81c386a8a4f584e44154c524
brittanyaylia <whitakerbrittany18@gmail.com>
2021-04-30 16:33:09 -0700

    code

--- Missions/scrape_mars.py
+++ Missions/scrape_mars.py
@@ -3,125 +3,97 @@
 
 # # Mission To Mars
 
-# In[1]:
-
-
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 from splinter import Browser
 import requests
 import pymongo
 
+def init_browser():
+    executable_path = {"executable_path":"/Users/brittanyaylia/Desktop/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
 
-# In[2]:
+mars_info = {}
 
+def news(browser):
+    browser = init_browser()
 
-#create path 
+    news_url = 'https://mars.nasa.gov/news/'
+    browser.visit(news_url)
+    html = browser.html
+    bsoup = bs(html, 'html.parser')
 
-executable_path = {"executable_path":"/Users/brittanyaylia/Desktop/chromedriver"}
+    titles = bsoup.find('div', class_='content_title').text
+    paragraphs = bsoup.find('div', class_='article_teaser_body').text
 
+    mars_info['titles'] = titles.text
+    mars_info['paragraphs'] = paragraphs.text
 
-# In[3]:
-
-
-browser = Browser("chrome", **executable_path, headless=False)
-
-
-# ## NASA Mars News
-
-# In[4]:
-
-
-#scrape mars news site
-
-news_url = 'https://mars.nasa.gov/news/'
-browser.visit(news_url)
-
-html = browser.html
-bsoup = bs(html, 'html.parser')
+    browser.quit()
+    return mars_info
 
 
-# In[5]:
+def featured_image(browser):
+    browser = init_browser()
+
+    img_url = 'https://spaceimages-mars.com/'
+    browser.visit(img_url)
+    img_html = browser.html
+    bsoup = bs(img_html, 'html.parser')
+
+    featured = bsoup.find("img", class_='fade-in').get('src')
+    featured_url = f'https://spaceimages-mars.com/{featured}'
+    featured_url
+
+    mars_info['featured_url'] = featured_url
+
+    browser.quit()
+    return mars_info
 
 
-#collect the latest news title and paragraph text
+def mars_facts(browser):
+    browser = init_browser
 
-titles = bsoup.find('div', class_='content_title').text
-print(titles)
-
-paragraphs = bsoup.find('div', class_='article_teaser_body').text
-print(paragraphs)
-
-
-# ## JPL Mars Space Images 
-
-# In[6]:
-
-
-#find the image url for the current featured mars image
-
-img_url = 'https://spaceimages-mars.com/'
-browser.visit(img_url)
-
-img_html = browser.html
-bsoup = bs(img_html, 'html.parser')
-
-featured = bsoup.find("img", class_='fade-in').get('src')
-featured_url = f'https://spaceimages-mars.com/{featured}'
-print(featured_url)
-
-
-# ## Mars Facts
-
-# In[7]:
-
-
-#scrape the table containing facts about mars including diameter, mass, etc.
-
-fax_url = "https://galaxyfacts-mars.com/"
-browser.visit(fax_url)
-
-fax_data = pd.read_html(fax_url)
-fax_data = pd.DataFrame(fax_data[0])
-
-mars_fax = fax_data.to_html(header= False, index = False)
-print(mars_fax)
-
-
-# ## Mars Hemispheres 
-
-# In[21]:
-
-
-#obtain urls and titles for high resolution images of mars hemispheres
-
-hemi_url = 'https://marshemispheres.com/'
-browser.visit(hemi_url)
-
-html = browser.html
-bsoup = bs(html, 'html.parser')
-
-items = bsoup.find_all('div', class_='item')
-images = []
-
-for item in items:
-    title = item.find('h3').text
-    print(title)
-    item_url = hemi_url + item.find('a')['href']
+    fax_url = "https://galaxyfacts-mars.com/"
+    browser.visit(fax_url)
     
-    browser.visit(item_url)
+    fax_data = pd.read_html(fax_url)
+    fax_data = pd.DataFrame(fax_data[0])
+    
+    mars_fax = fax_data.to_html(header= False, index = False)
+    mars_info['mars_facts'] = mars_fax
+
+    browser.quit()
+    return mars_info
+
+
+def hemispheres(browser):
+    browser = init_browser()
+    
+    hemi_url = 'https://marshemispheres.com/'
+    browser.visit(hemi_url)
     html = browser.html
     bsoup = bs(html, 'html.parser')
     
-    image_url = hemi_url + bsoup.find('img', class_='wide-image').get('src')
-    print(image_url)
-    
-    insert = dict({"Title": title, "url": image_url})
-    images.append(insert)
+    items = bsoup.find_all('div', class_='item')
+    images = []
 
+    for item in items:
+        title = item.find('h3').text
+        item_url = hemi_url + item.find('a')['href']
+        
+        browser.visit(item_url)
+        html = browser.html
+        bsoup = bs(html, 'html.parser')
+        
+        image_url = hemi_url + bsoup.find('img', class_='wide-image').get('src')
+        
+        insert = dict({"Title": title, "url": image_url})
+        images.append(insert)
 
-# In[ ]:
+    mars_info['hemisphere_images'] = images
 
-
+    browser.quit()
+    return mars_info
 
 
